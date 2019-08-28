16d1eb66d58481cd27661a2a26a1a8cc8d7088e0
zhuravel stanislav <stanislav.zhuravel@vitech.com.ua>
2019-08-28 22:39:42 +0300

    owner  controller added

--- pet-clinic-web/src/main/java/com/stas/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/stas/mypetclinic/controllers/OwnerController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VetController {
+@RequestMapping("/owners")
+public class OwnerController {
 
-@RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    @RequestMapping({"","/","/index","/index.html"})
     public String listVets() {
-        return "vets/index";
+        return "owners/index";
     }
 
 }
