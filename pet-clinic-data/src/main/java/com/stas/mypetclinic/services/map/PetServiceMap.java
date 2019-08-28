f3c027213b03ecf20aa6839f2663dbca5a86c819
zhuravel stanislav <stanislav.zhuravel@vitech.com.ua>
2019-08-28 21:38:55 +0300

    map based services added

---
+++ pet-clinic-data/src/main/java/com/stas/mypetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.stas.mypetclinic.services.map;
+
+import com.stas.mypetclinic.model.Pet;
+import com.stas.mypetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
