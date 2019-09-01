a6ad8e3173fcc863131c2eb2a6fd8dce77066587
zhuravel stanislav <stanislav.zhuravel@vitech.com.ua>
2019-09-01 15:54:09 +0300

    fixed links

--- pet-clinic-web/src/main/java/com/stas/mypetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/stas/mypetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.stas.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +10,12 @@ public class IndexController {
     @RequestMapping({"","/","index","index.html"})
     public String index(){
         return "index";
+    }
+
+    @RequestMapping({"/oups"})
+    public String findOwners(Model model) {
+        return "owners/notimplemented";
+
     }
 
 }
