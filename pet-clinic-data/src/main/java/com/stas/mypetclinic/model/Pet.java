a90dd8c14ca194d0c8b6b6d3fb5ed27284d14958
zhuravel stanislav <stanislav.zhuravel@vitech.com.ua>
2019-09-14 23:16:32 +0300

    appgrade to java 11

--- pet-clinic-data/src/main/java/com/stas/mypetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/stas/mypetclinic/model/Pet.java
@@ -22,8 +22,9 @@ public class Pet extends BaseEntity {
         this.name = name;
         this.petType = petType;
         this.owner = owner;
-        this.birthDate = birthDate;
-        this.visits = visits;
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
     }
 
     @Column(name = "name")
